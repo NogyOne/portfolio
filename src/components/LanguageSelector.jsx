38d496d36a3714db44a8b4e734191a676ec88562
Germán Gómez <german_gomez1205@hotmail.com>
2025-03-04 15:26:06 -0700

    update. Home & Header section

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -13,16 +13,16 @@ export default function LanguageSelector() {
   };
 
   return (
-    <div className="absolute top-10 left-10">
+    <div className="p-2 border-gray-400 border bg-[#222E44] rounded-full w-12 h-12 transform hover:scale-x-125 hover:scale-y-125 transition duration-300 ease-out hover:border-[#279EFF]">
       <img
         src="/icons/lang.svg"
         alt=""
-        className="size-10 cursor-pointer"
+        className="m-auto rounded-full size-8 cursor-pointer"
         onClick={toggleDropdown}
       />
 
       {dropVisible && (
-        <div className="flex flex-col items-center justify-center w-full border rounded  mt-3 drop-shadow-2xl backdrop-blur-lg bg-[#3f519480]">
+        <div className="flex flex-col items-center justify-center w-28 border rounded  mt-3 drop-shadow-2xl backdrop-blur-lg bg-[#3f519480]">
           {languages.map((language) => (
             <a
               key={language}
